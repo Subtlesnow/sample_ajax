15c35545d77a062ff61fe84707639782da96ddd2
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2018-05-22 14:36:05 -0400

    Ajax complete

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const countryDataEl = document.getElementById('country-data');
+
+const clearCountryData = () => {
+  countryDataEl.innerHTML = '';
+}
+
+const displayCountryData = country => {
+  const countryNameEl = document.createElement('h3');
+  countryNameEl.innerHTML = country.name;
+  const countryNativeEl = document.createElement('h4');
+  countryNativeEl.innerHTML = country.nativeName;
+  const countryCapitalEl = document.createElement('p');
+  countryCapitalEl.innerHTML = `Capital: ${country.capital}`
+  const countryPopEl = document.createElement('p');
+  countryPopEl.innerHTML = `Population: ${country.population}`
+
+  countryDataEl.appendChild(countryNameEl);
+  countryDataEl.appendChild(countryNativeEl);
+  countryDataEl.appendChild(countryCapitalEl);
+  countryDataEl.appendChild(countryPopEl);
+}
+
 window.addEventListener('load', event => {
   console.log('㊋ estonia-ajax: DOM is ready to AJAXX! ㊋');
   document.getElementById('find-country').addEventListener('click', event => {
@@ -6,6 +28,8 @@ window.addEventListener('load', event => {
     axios.get(`https://restcountries.eu/rest/v2/name/${searchCountry}`)
       .then( response => {
         const country = response.data[0];
+        clearCountryData();
+        displayCountryData( country );
         console.log( country.nativeName, country.capital, country.population );
       })
       .catch( error => { console.error( error ); });
